38b36220f72b7d7e18d00a026e5c59b1e238a229
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-03-17 01:23:43 -0400

    Created script to use ComputeBuffer to another vertex Shader and a PCR (WIP)
    Currently PCR expects 3D so crash when pased a 2D buffer without matching size

---
+++ Assets/Visuals/Shaders/ComputeShader/ComputeToVertex.cs
@@ -0,0 +1,17 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace Visuals.Shaders.ComputeShader
+{
+    public class ComputeToVertex : MonoBehaviour
+    {
+        [SerializeField] private ComputeShaderHandler _compute;
+        [SerializeField] private PointCloudRendererSimple _pcs;
+        [Button]
+        void Bind()
+        {
+            var buff = _compute.Buffer;
+            _pcs.SetBuffer(buff);
+        }
+    }
+}
