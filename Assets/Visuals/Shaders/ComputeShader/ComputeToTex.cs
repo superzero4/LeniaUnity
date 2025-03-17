a18757f415cc26ec1f209b7e430ceee6ecb5cfbc
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-03-17 01:23:02 -0400

    Cretad script to read the compute buffer to a texture for direct debugging

---
+++ Assets/Visuals/Shaders/ComputeShader/ComputeToTex.cs
@@ -0,0 +1,37 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace Visuals.Shaders.ComputeShader
+{
+    public class ComputeToTex : MonoBehaviour
+    {
+        [SerializeField] private ComputeShaderHandler _compute;
+        [SerializeField] private Material _material;
+        [SerializeField] private FilterMode _filterMode;
+        private Texture2D _texture;
+
+        [Button]
+        private void UpdateTexture()
+        {
+            if (_compute.Buffer == null)
+                return;
+            if (_texture == null || _texture.width != _compute.Size.x || _texture.height != _compute.Size.y)
+            {
+                Destroy(_texture);
+                _texture = new Texture2D(_compute.Size.x, _compute.Size.y, TextureFormat.RFloat, false);
+                _texture.wrapMode = TextureWrapMode.Clamp;
+                _texture.filterMode = _filterMode;
+            }
+            float[] data = new float[_compute.Size.x * _compute.Size.y * _compute.Size.z];
+            _compute.Buffer.GetData(data);
+            _texture.SetPixelData(data, 0);
+            _texture.Apply();
+            _material.SetTexture("_MainTex", _texture);
+        }
+
+        private void Update()
+        {
+            UpdateTexture();
+        }
+    }
+}
